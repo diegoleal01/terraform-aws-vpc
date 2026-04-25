c261166bf3d5bbc142a10e3ba591f085e1b0be0e
Diego Leal <souza.dleal@gmail.com>
2026-04-24 21:12:31 -0300

    feat(vpc): add outputs for vpc id, subnet ids and gateway

--- modules/route/output.tf
+++ modules/route/output.tf
@@ -1,7 +1,9 @@
 output "private_rtb_id" {
-  value = aws_route_table.private_rtb.id
+  value       = aws_route_table.private_rtb.id
+  description = "The ID of the private route table"
 }
 
 output "public_rtb_id" {
-  value = aws_route_table.public_rtb.id
+  value       = aws_route_table.public_rtb.id
+  description = "The ID of the public route table"
 }
