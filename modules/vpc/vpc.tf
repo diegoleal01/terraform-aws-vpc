09432e4a917b51537e4a10ebcb5c13662ff09e54
Diego Leal <souza.dleal@gmail.com>
2026-05-25 17:05:45 -0300

    fix(vpc): remove undeclared variables from aws_vpc resource

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -8,8 +8,8 @@ locals {
 
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr_block
-  enable_dns_hostnames = var.vpc_dns_hostnames
-  enable_dns_support   = var.vpc_dns_support
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = merge(
     var.tags,
